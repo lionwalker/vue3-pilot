1f79ce795866c3753ede7ced251ace08c21e564e
Lahiru Madusanka <hhlahirumadusanka@gmail.com>
2023-10-16 23:49:24 +0530

    teleport

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>{{ title }}</h1>
   <p>Welcome ...... </p>
-  <div v-if="showModal">
+  <teleport to=".modals" v-if="showModal">
     <Modal theme="sale" @close="toggleModal">
       <template v-slot:links>
         <a href="#">sign up now</a>
@@ -11,13 +11,13 @@
       <h1>{{ header }}</h1>
       <p>{{ text }}</p>
     </Modal>
-  </div>
-  <div v-if="showModalTwo">
+  </teleport>
+  <teleport to=".modals" v-if="showModalTwo">
     <Modal theme="sale" @close="toggleModalTwo">      
       <h1>sign up to the newsletter</h1>
       <p>for updates and promo codes!</p>
     </Modal>
-  </div>
+  </teleport>
   <button @click.alt="toggleModal">open modal (alt)</button>
   <button @click="toggleModalTwo">open modal</button>
 </template>
@@ -51,7 +51,7 @@ export default {
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
