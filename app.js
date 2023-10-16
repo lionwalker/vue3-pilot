1c9fd0f8e8ec5b9026ca817d45bb965168772784
Lahiru Madusanka <hhlahirumadusanka@gmail.com>
2023-10-16 20:24:11 +0530

    mouse events

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brndon Sanderson',
-            age: 45
+            age: 45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
@@ -13,6 +15,16 @@ const app = Vue.createApp({
         },
         toggleShowBook() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(event, data) {
+            console.log('event', event.type);
+            if (data) {
+                console.log(data);
+            }
+        },
+        handleMousemove(event) {
+            this.x = event.offsetX
+            this.y = event.offsetY
         }
     }
 });
