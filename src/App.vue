7765a3bf8bea079a00a706c2ac9d07bff033622c
Lahiru Madusanka <hhlahirumadusanka@gmail.com>
2023-10-16 23:39:59 +0530

    event modifiers and slots

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,29 @@
   <h1>{{ title }}</h1>
   <p>Welcome ...... </p>
   <div v-if="showModal">
-    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
+    </Modal>
+  </div>
+  <div v-if="showModal2">
+    <Modal theme="sale" @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      
+      <h1>Second modal</h1>
+      <p>Text of the second modal</p>
+    </Modal>
   </div>
   <button @click="toggleModal">open modal</button>
+  <button @click="toggleModal2">open modal 2</button>
 </template>
 
 <script>
@@ -17,12 +37,16 @@ export default {
       title: 'My first Vue app',
       header: 'Sign up for the Giveaway!',
       text: 'Grab your walker\'s swag for half price!',
-      showModal: false
+      showModal: false,
+      showModal2: false,
     }
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2
     }
   },
   components: {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="backdrop" @click="closeModal">
+    <div class="backdrop" @click.self="closeModal">
         <div class="modal" :class="{ sale: theme === 'sale' }">
-            <h1>{{ header }}</h1>
-            <p>{{ text }}</p>
+            <slot>default content</slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['header', 'text', 'theme'],
+    props: ['theme'],
     methods: {
         closeModal() {
             this.$emit('close')
@@ -19,38 +21,55 @@ export default {
 </script>
 
 <style>
-.modal {
+  .modal {
     width: 400px;
     padding: 20px;
-    margin: 100px auto;
+    margin: 200px auto;
     background: white;
     border-radius: 10px;
-}
-
-.backdrop {
+  }
+  .backdrop {
     top: 0;
     position: fixed;
-    background: rgba(0, 0, 0, 0.5);
+    background: rgba(0,0,0,0.5);
     width: 100%;
     height: 100%;
-}
-
-.modal h1 {
+  }
+  .modal h1 {
     color: #03cfb4;
     border: none;
     padding: 0;
-}
-
-.modal p {
+  }
+  .modal p {
     font-style: normal;
-}
+  }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
 
-.modal.sale {
+  /* sale styles */
+  .modal.sale {
     background: crimson;
     color: white;
-}
-
-.modal.sale h1 {
+  }
+  .modal.sale h1 {
     color: white;
-}
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
+    color: white;
+  }
+
 </style>
