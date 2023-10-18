0fe5a53e12694291f097a8893dff12c5f4d3b354
Lahiru Madusanka <lahiru@mylinex.com>
2023-10-18 16:54:22 +0530

    using refs for reactivity

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,28 @@
 <template>
   <div class="home">
     <h2>Home</h2>
-    <p ref="reference">My name is {{ name }} and my age is {{ age }}</p>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
+    <button @click="age++">Increse age</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 export default {
   name: 'HomeView',
   setup() {
     
-    const reference = ref(null);
-
-    let name = 'mario'
-    let age = 30
+    const name = ref('mario')
+    const age = ref(30)
 
     const handleClick = () => {
-      console.log(reference, reference.value);
-      reference.value.classList.add('test')
-      reference.value.textContent = 'hello, ninjas'
+      name.value = 'lahiru'
+      age.value = 35
     }
 
-    return { name, age, handleClick, reference }
+    return { name, age, handleClick }
   }
 }
 </script>
